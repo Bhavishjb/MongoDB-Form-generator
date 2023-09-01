10fc3d98f081afe732162907f2a2786135be9704
Bhavishjb <bhavishkarkera22@gmail.com>
2023-09-01 11:58:51 +0530

    chsnged size/length as type number and created folder for js

--- application/controllers/HomeController.php
+++ application/controllers/HomeController.php
@@ -1,18 +1,19 @@
 <?php
+
 defined('BASEPATH') or exit('No direct script access allowed');
 
 class HomeController extends CI_Controller
 {
-  public function __construct()
-  {
-    parent::__construct();
-  }
+    public function __construct()
+    {
+        parent::__construct();
+    }
 
-  public function index()
-  {
-    
-    $this->load->view('templates/header');
-    $this->load->view('pages/home');
-    $this->load->view('templates/footer');
-  }
+    public function index()
+    {
+
+        $this->load->view('templates/header');
+        $this->load->view('pages/home');
+        $this->load->view('templates/footer');
+    }
 }
